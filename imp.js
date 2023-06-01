71135f189eea38de2b7a6b587f33a20d3ea6a5fa
Mayank-Lad <ladmayank15@gmail.com>
2023-06-01 12:11:40 +0530

    portfolio-website

---
+++ imp.js
@@ -0,0 +1,20 @@
+
+function myfunction1(){
+   count=0;
+   count=count+1;
+   var element = document.body;
+   element.classList.toggle("lightmode");
+   document.getElementById('b1').style.display="block";
+   document.getElementById('b2').style.display="none";
+
+   
+}
+
+function myfunction2(){
+   count=0;
+   count=count+1;
+   var element = document.body;
+   element.classList.toggle("lightmode");
+   document.getElementById('b2').style.display="block";
+   document.getElementById('b1').style.display="none";   
+}
